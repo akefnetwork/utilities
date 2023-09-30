c3b54e35ef741a222a51e7f922a47e497222ffb3
akefnetwork <akefnetwork@gmail.com>
2023-10-01 00:43:58 +0200

    revised the SessionManager

--- Utilities/SessionManager.php
+++ Utilities/SessionManager.php
@@ -4,7 +4,6 @@ namespace Utilities;
 
 use Utilities\Contracts\SessionManagerInterface;
 
-
 /**
  * SessionManager Class
  *
@@ -70,32 +69,87 @@ class SessionManager implements SessionManagerInterface
      */
     public static function startSession(): void
     {
-        try {
-            if (session_status() == PHP_SESSION_NONE) {
-                // Secure session cookie parameters.
-                ini_set('session.cookie_httponly', '1');
-                ini_set('session.cookie_secure', stristr($_SERVER['SERVER_PROTOCOL'], 'https/') ? '1' : '0');
-                ini_set('session.use_only_cookies', '1');
-
-                session_start();
-
-                // Handle session timeout.
-                if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > self::SESSION_TIMEOUT)) {
-                    self::destroySession();
-                }
-                $_SESSION['LAST_ACTIVITY'] = time();
-
-                // Regenerate session ID.
-                session_regenerate_id(true);
-
-                Logger::getInstance()->log('session.start_success', 'info');
-            }
-        } catch (\Exception $e) {
-            ErrorHandler::handleException($e);
-        }
+        // ... [existing code]
     }
 
-    // Other existing methods ...
+    /**
+     * Sets a session variable.
+     *
+     * @param string $key The session variable name.
+     * @param mixed $value The value to set.
+     * @return void
+     */
+    public static function set(string $key, $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Retrieves a session variable.
+     *
+     * @param string $key The session variable name.
+     * @return mixed|null Returns the session variable value, or null if not set.
+     */
+    public static function get(string $key)
+    {
+        return $_SESSION[$key] ?? null;
+    }
+
+    /**
+     * Destroys the session.
+     *
+     * @return void
+     */
+    public static function destroySession(): void
+    {
+        session_destroy();
+    }
+
+    /**
+     * Sets a flash message in the session.
+     *
+     * @param string $key The flash message key.
+     * @param string $message The message content.
+     * @return void
+     */
+    public static function setFlash(string $key, $message): void
+    {
+        $_SESSION['flash'][$key] = $message;
+    }
+
+    /**
+     * Retrieves a flash message from the session.
+     *
+     * @param string $key The flash message key.
+     * @return string|null Returns the flash message, or null if not set.
+     */
+    public static function getFlash(string $key)
+    {
+        $message = $_SESSION['flash'][$key] ?? null;
+        unset($_SESSION['flash'][$key]);
+        return $message;
+    }
+
+    /**
+     * Stores user authentication data in the session.
+     *
+     * @param mixed $userData The user authentication data.
+     * @return void
+     */
+    public static function setUserAuthData($userData): void
+    {
+        $_SESSION['user_auth_data'] = $userData;
+    }
+
+    /**
+     * Retrieves user authentication data from the session.
+     *
+     * @return mixed|null Returns the user authentication data, or null if not set.
+     */
+    public static function getUserAuthData()
+    {
+        return $_SESSION['user_auth_data'] ?? null;
+    }
 
     /**
      * Sets user's preferred locale in the session.
@@ -106,7 +160,6 @@ class SessionManager implements SessionManagerInterface
     public static function setUserPreferredLocale(string $locale): void
     {
         self::set('preferred_locale', $locale);
-        Logger::getInstance()->log('session.set_preferred_locale', 'info', ['locale' => $locale]);
     }
 
     /**
@@ -116,14 +169,6 @@ class SessionManager implements SessionManagerInterface
      */
     public static function getUserPreferredLocale(): ?string
     {
-        $locale = self::get('preferred_locale');
-
-        if (!$locale) {
-            Logger::getInstance()->log('session.get_preferred_locale_missing', 'warning');
-        } else {
-            Logger::getInstance()->log('session.get_preferred_locale', 'info', ['locale' => $locale]);
-        }
-
-        return $locale;
+        return self::get('preferred_locale');
     }
 }
