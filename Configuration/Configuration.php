815e84e6a1f4f714def69373648c7e918333f607
akefnetwork <akefnetwork@gmail.com>
2023-10-01 21:13:12 +0200

    refactored the Configuration file to public static method get() to access the configuration values, Replacing the global env function with a private static method of the Configuration class to maintain encapsulation, and Wrapping the configuration array in a class structure with the namespace Configuration.

--- Configuration/Configuration.php
+++ Configuration/Configuration.php
@@ -3,9 +3,9 @@
 namespace Configuration;
 
 /**
- * Application Configuration File
+ * Application Configuration Class
  * 
- * This file is used to store various configuration settings for the application.
+ * This class is used to store and retrieve various configuration settings for the application.
  * It includes settings related to the application environment, debugging,
  * URL, locale, timezone, and utility paths.
  *
@@ -14,13 +14,68 @@ namespace Configuration;
  * @author   Brahim Akef <b@akef.net>
  * @link     http://github.com/akefnetwork
  */
+class Configuration {
 
-// Define the application root directory.
-define('APP_ROOT', dirname(__DIR__));
+    // Define the application root directory.
+    private const APP_ROOT = dirname(__DIR__);
 
-// Check if the env function exists, if not, define it.
-if (!function_exists('env')) {
-    function env($key, $default = null) {
+    /**
+     * All application configurations are stored in this array.
+     */
+    private static $configurations = [
+        // ... (all your configurations) ...
+
+        'session' => [
+            'timeout' => self::env('SESSION_TIMEOUT', 1800), // 30 minutes
+            'cookie_httponly' => true,
+            'cookie_secure' => self::env('SESSION_COOKIE_SECURE', false), // Set to true if using HTTPS
+        ],
+
+        'error_handling' => [
+            'display_errors' => self::env('DISPLAY_ERRORS', false),
+            // ... other error handling configurations ...
+        ],
+
+        'available_locales' => ['en', 'es', 'fr'],
+
+        'paths' => [
+            'errorHandler' => self::APP_ROOT . '/Utilities/ErrorHandler.php',
+            'logger' => self::APP_ROOT . '/Utilities/Logger.php',
+            'localeHandler' => self::APP_ROOT . '/Utilities/LocaleHandler.php',
+            'uploadDir' => self::APP_ROOT . '/uploads', // Ensure correct permissions are set for this directory.
+            // ... additional utility paths ...
+        ],
+
+        'localization' => [
+            'available_locales' => ['en', 'es', 'fr'],
+            'default_locale' => self::env('APP_LOCALE', 'en'),
+            'translations_path' => self::APP_ROOT . '/lang', // Ensure this directory exists and contains translation files.
+        ],
+
+        'logFilePath' => '../Logs/app.log',
+
+        // ... other configurations ...
+    ];
+
+    /**
+     * Retrieve a configuration value by its key.
+     * 
+     * @param string $key The key of the configuration to retrieve.
+     * @param mixed $default The default value to return if the key isn't found.
+     * @return mixed The configuration value.
+     */
+    public static function get($key, $default = null) {
+        return static::$configurations[$key] ?? $default;
+    }
+
+    /**
+     * Utility function to retrieve environment variables or return a default value.
+     * 
+     * @param string $key The key of the environment variable to retrieve.
+     * @param mixed $default The default value to return if the environment variable isn't found.
+     * @return mixed The value of the environment variable.
+     */
+    private static function env($key, $default = null) {
         $value = getenv($key);
         if ($value === false) {
             return $default;
@@ -28,89 +83,3 @@ if (!function_exists('env')) {
         return $value;
     }
 }
-
-return [
-    // ... (other configurations remain the same) ...
-
-    /**
-     * --------------------------------------------------------------------------
-     * Session Configuration
-     * --------------------------------------------------------------------------
-     * 
-     * Here you may define all of the session settings for your application including
-     * session timeout, cookie settings, and others.
-     * 
-     */
-    'session' => [
-        'timeout' => env('SESSION_TIMEOUT', 1800), // 30 minutes
-        'cookie_httponly' => true,
-        'cookie_secure' => env('SESSION_COOKIE_SECURE', false), // Set to true if using HTTPS
-    ],
-
-    /**
-     * --------------------------------------------------------------------------
-     * Error Handling Configuration
-     * --------------------------------------------------------------------------
-     * 
-     * Configuration settings related to how errors and exceptions are handled
-     * in your application. This includes log settings, display settings, etc.
-     * 
-     */
-    'error_handling' => [
-        'display_errors' => env('DISPLAY_ERRORS', false),
-        // ... other error handling configurations ...
-    ],
-
-    /**
-     * --------------------------------------------------------------------------
-     * Available Locales
-     * --------------------------------------------------------------------------
-     * 
-     * All available locales for the application. You should update this array
-     * with any additional locales your application will support. Ensure that
-     * the application logic is implemented to handle these locales correctly.
-     */
-    'available_locales' => ['en', 'es', 'fr'],
-
-    /**
-     * --------------------------------------------------------------------------
-     * Utility Paths
-     * --------------------------------------------------------------------------
-     * 
-     * This section defines the absolute file paths to various utility classes
-     * used throughout the application. Ensure that all parts of your
-     * application that need these paths import them from this configuration file.
-     * 
-     * Each utility class, such as ErrorHandler, Logger, etc., has its own
-     * designated path entry, making it easy to reference them individually
-     * wherever needed.
-     * 
-     */
-    'paths' => [
-        'errorHandler' => APP_ROOT . '/Utilities/ErrorHandler.php',
-        'logger' => APP_ROOT . '/Utilities/Logger.php',
-        'localeHandler' => APP_ROOT . '/Utilities/LocaleHandler.php',
-        'uploadDir' => APP_ROOT . '/uploads', // Ensure correct permissions are set for this directory.
-        // ... additional utility paths ...
-    ],
-
-    /**
-     * --------------------------------------------------------------------------
-     * Localization Configuration
-     * --------------------------------------------------------------------------
-     * 
-     * Configuration settings related to localization and internationalization 
-     * of the application. This includes the available locales, default locale, 
-     * and the path to the translations files.
-     * 
-     */
-    'localization' => [
-        'available_locales' => ['en', 'es', 'fr'],
-        'default_locale' => env('APP_LOCALE', 'en'),
-        'translations_path' => APP_ROOT . '/lang', // Ensure this directory exists and contains translation files.
-    ],
-
-    'logFilePath' => '../Logs/app.log',
-
-    // ... other configurations ...
-];
